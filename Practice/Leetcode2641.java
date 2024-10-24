1e8e2c7e68df50d160486adc9a24e05a089d653d
pkbtc <parasarkashyap06@gmail.com>
2024-10-24 14:28:57 +0530

    Flip Equivalent Binary Trees

--- Practice/Leetcode2641.java
+++ Practice/Leetcode2641.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class Leetcode2641 {
     public class TreeNode {
-             int val;
-             TreeNode left;
-             TreeNode right;
-             TreeNode() {}
-            TreeNode(int val) { this.val = val; }
-            TreeNode(int val, TreeNode left, TreeNode right) {
-                this.val = val;
-                this.left = left;
-                this.right = right;
-            }
-         }
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
     public static void main(String[] args) {
             
     }
