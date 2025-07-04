b50ddb418de2bbe4820b374bc993f15fc0101c0e
pkbtc <parasarkashyap06@gmail.com>
2025-07-04 21:40:45 +0530

    3307. Find the K-th Character in String Game II

---
+++ Practice/Leetcode3307.java
@@ -0,0 +1,15 @@
+class Leetcode3307{
+    public static void main(String[] args) {
+        
+    }
+    public char kthCharacter(long k, int[] operations) {
+        int countOps=0;
+        long val=k;
+        while(val>1){
+            int jumps = (int) Math.ceil(Math.log(val) / Math.log(2));
+            val -= (long) Math.pow(2, jumps - 1);
+            countOps += operations[jumps - 1];
+        }
+        return (char) ('a' + (countOps % 26));
+    }
+}
